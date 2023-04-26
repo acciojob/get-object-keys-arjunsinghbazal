069c50b6e875a0cc1ab444a9e10eee582f1126f0
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-04-26 14:55:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-//your JS code here. If required.
-let student = {name:arjun,Age:14};
-function getKeys(){
-	return student.key;
-}
+// Create the student object with a name property
+const student = {
+  name: "John Doe"
+};
+
+// Add the getKeys() method to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Call the getKeys() method on the student object
+const keys = student.getKeys();
+
+// Log the keys array to the console
+console.log(keys); // Output: ["name"]
